f5613ca7915d1566f9b8316009683bd000a374bc
Adrien <adrien.boustie@gmail.com>
2025-04-14 16:00:11 +0200

    update test

--- ORM/test_ORM.py
+++ ORM/test_ORM.py
@@ -1,7 +1,7 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from ORM import Base, Utilisateur 
+from ORM import Utilisateur 
 
 # Fixture for setting up and tearing down the database
 @pytest.fixture
