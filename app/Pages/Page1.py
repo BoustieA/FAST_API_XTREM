76bd5d3178db5b6e2236890ccf67f6561e852f81
BoustieA <124263630+BoustieA@users.noreply.github.com>
2025-04-15 16:44:54 +0200

    Delete app directory

--- app/Pages/Page1.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-if not st.session_state.get("pswd_check", False):
-    st.warning("Veuillez vous connecter depuis la page d'accueil.")
-    st.stop()
-
-st.title("Connecté")
