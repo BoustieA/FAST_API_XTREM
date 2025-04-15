38574fb50dffacd8515708055ba374cefb6e4ad8
Adrien <adrien.boustie@gmail.com>
2025-04-15 09:57:21 +0200

    commit 1st file

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 class Utilisateur(Base):
     __tablename__ = "Utilisateur"
 
-    Id_Utilisateur = Column(Integer, 
+    Id_Utilisateur = Column(Integer,
                             primary_key=True, autoincrement=True)
     nom = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False)
