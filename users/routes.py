1395d4dab58330428e2524d0451bed884746e1ec
Niels-Patrick <73878385+Niels-Patrick@users.noreply.github.com>
2025-04-15 10:56:44 +0200

    Updates users
    - Correction des erreurs logiques dans la route d'authentification dans routes.py.
    - Ajout de la route de création d'utilisateur dans routes.py.
    - Correction d'appel de méthode dans test_routes.py (get_db au lieu de connect).

--- users/routes.py
+++ users/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
-from ORM import UtilisateurBase, Utilisateur
+from models.user import UtilisateurBase, Utilisateur
 from sqlalchemy.orm import Session
 from db.db_manager import connect, diconnect, get_db
 import hashlib
@@ -33,22 +33,21 @@ async def login(data: UtilisateurBase, db: Session = Depends(get_db)):
 
             connect()
 
-            utilisateurs = db.query(Utilisateur).filter_by(nom=nom).first()
+            utilisateur = db.query(Utilisateur).filter_by(nom=nom).first()
 
-            if utilisateurs:
-                for utilisateur in utilisateurs:
-                    decrypted_password = utilisateur.pswd.decode()
-                    if (
-                        utilisateur.nom == nom and
-                        decrypted_password == hashlib.sha256(
-                            pswd.encode()
-                            ).hexdigest()
-                    ):
-                        return JSONResponse(
-                            content={
-                                "message": "Succès : utilisateur authientifié"
-                                }
-                            ), 200
+            if utilisateur:
+                encoded_password = utilisateur.pswd.encode()
+                if (
+                    utilisateur.nom == nom and
+                    encoded_password == hashlib.sha256(
+                        pswd.encode()
+                    ).hexdigest()
+                ):
+                    return JSONResponse(
+                        content={
+                            "message": "Succès : utilisateur authientifié"
+                            }
+                        ), 200
 
                 return JSONResponse(
                     content={
@@ -82,3 +81,49 @@ async def logout():
     Route pour la déconnexion
     """
     os._exit(0)
+
+
+@router.post("/add-user")
+async def add_user(data: UtilisateurBase, db: Session = Depends(get_db)):
+    """
+    Route pour l'ajout d'un utilisateur dans la bdd
+
+    Paramètres :
+        - data: les informations envoyées par l'utilisateur
+        - db: la session de bdd actuelle
+    """
+    try:
+        if data:
+            connect()
+
+            utilisateur = db.query(Utilisateur).filter_by(nom=data.nom).first()
+
+            if utilisateur:
+                return JSONResponse(
+                    content={
+                        "message": "Erreur : nom d'utilisateur déjà existant"
+                        }
+                    ), 403
+            
+            hashed_pswd = hashlib.sha256(data.pswd.encode()).hexdigest()
+
+            db_utilisateur = Utilisateur(
+                nom = data.nom,
+                email = data.email,
+                pswd = hashed_pswd
+            )
+
+            db.add(db_utilisateur)
+            db.commit()
+
+            return JSONResponse(
+                content={
+                    "message": "Succès : nouvel utilisateur enregistré"
+                    }
+                ), 200
+            
+    except Exception as e:
+        return JSONResponse(content={"message": "Erreur : " + str(e)}), 500
+    
+    finally:
+        diconnect(db)
--- users/test_routes.py
+++ users/test_routes.py
@@ -1,14 +1,14 @@
 import pytest
 from fastapi import Depends
 from routes import login
-from db.database import connect
+from db.database import get_db
 
 
 @pytest.fixture
 def test_login_success():
     data = {"nom": "Alice", "pswd": "secure123"}
 
-    response = login(data, Depends(connect))
+    response = login(data, Depends(get_db))
 
     assert response.status_code == 200
 
@@ -16,7 +16,7 @@ def test_login_success():
 def test_login_incorrect():
     data = {"nom": "Alice", "pswd": "wrongpassword"}
 
-    response = login(data, Depends(connect))
+    response = login(data, Depends(get_db))
 
     assert response.status_code == 401
 
@@ -24,7 +24,7 @@ def test_login_incorrect():
 def test_login_not_found():
     data = {"nom": "Jack", "pswd": "secure123"}
 
-    response = login(data, Depends(connect))
+    response = login(data, Depends(get_db))
 
     assert response.status_code == 404
 
@@ -32,6 +32,6 @@ def test_login_not_found():
 def test_login_no_data():
     data = {}
 
-    response = login(data, Depends(connect))
+    response = login(data, Depends(get_db))
 
     assert response.status_code == 400
