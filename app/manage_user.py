a838aaaa1553e5b0e6e91a3fbd02f573dcb96304
Adrien <adrien.boustie@gmail.com>
2025-04-16 14:12:00 +0200

    update typo

--- app/manage_user.py
+++ app/manage_user.py
@@ -1,24 +1,24 @@
 import requests
 
 
-API_adress="http://127.0.0.1:8000"
+API_adress = "http://127.0.0.1:8000"
 
 
-def check_authentity(nom,pswd):
-    json = {"nom": nom,"pswd": pswd}
+def check_authentity(nom, pswd):
+    json = {"nom": nom, "pswd": pswd}
     response = requests.post(API_adress+"/login", json=json)
     code = response.status_code
-    if code==200:
+    if code == 200:
         return True
     else:
         return False
 
 
 def user_exist(nom):
-    json = {"nom": nom,"pswd": ""}
+    json = {"nom": nom, "pswd": ""}
     response = requests.post(API_adress+"/login", json=json)
     code = response.status_code
-    if code==401:
+    if code == 401:
         return True
     return False
 
@@ -27,20 +27,20 @@ def create_user(nom, pswd, mail):
     json = {"nom": nom,
           "email": mail,
           "pswd": pswd}
-    requests.post(API_adress+"/add_user", json=json)
+    requests.post(API_adress + "/add_user", json=json)
 
 
 def email_exist(mail):
     json = {"email": mail}
-    return requests.post(API_adress+"/mail_exist", json=json)
+    return requests.post(API_adress + "/mail_exist", json=json)
 
-def update_pswd(pswd,mail):
-    nom = requests.post(API_adress+"/get_name_from_mail", json=json)
+def update_pswd(pswd, mail):
+    nom = requests.post(API_adress + "/get_name_from_mail", json=json)
 
     json={"nom": nom,
           "mail": mail,
           "pswd": pswd}
-    requests.post(API_adress+"/update-user", json)
+    requests.post(API_adress + "/update-user", json)
     
 
 def check_pswd_security_level(mdp):
@@ -51,17 +51,17 @@ def check_pswd_security_level(mdp):
     security_spec = False
     for i in mdp:
         if security_number == False:
-            if ord(i)<58 and ord(i)>47:
+            if ord(i) < 58 and ord(i) > 47:
                 security += 1
                 security_number = True
         
         if security_minus == False:
-            if ord(i)<123 and ord(i)>96:
+            if ord(i) < 123 and ord(i) > 96:
                 security += 1 
                 security_minus = True
         
         if security_maj == False:
-            if ord(i)<91 and ord(i)>64:
+            if ord(i) < 91 and ord(i) > 64:
                 security += 1
                 security_maj = True
         
